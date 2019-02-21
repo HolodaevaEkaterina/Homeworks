95f00944c70b422c9542d25eb719cfe2e1e00c38
Ekaterina <kata_1998@mail.ru>
2019-02-21 16:41:49 +0300

    added 2 task

---
+++ Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,14 @@
+﻿let count = System.Console.ReadLine() |> int 
+let rec fibonacci n =
+    match n with
+    | 1 | 2 -> 1
+    | _ -> fibonacci (n-1) + fibonacci (n-2)
+
+let rec printFibonacci n  =
+    match n with 
+    | 1 -> printf "%d, " (fibonacci (n))
+    | _ -> printFibonacci (n-1)
+           printf "%d, " (fibonacci (n))
+           
+
+printFibonacci(count)
