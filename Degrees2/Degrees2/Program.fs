c8ba0c37899f4564167f4d540d3455bffd38c5c1
Ekaterina <kata_1998@mail.ru>
2019-02-22 20:21:59 +0300

    added 4 task

---
+++ Degrees2/Degrees2/Program.fs
@@ -0,0 +1,14 @@
+﻿let n = System.Console.ReadLine() |> int 
+let m = System.Console.ReadLine() |> int 
+
+if (m + n < 32) && (n > 0) && (m > 0) then 
+        
+        let rec degreeList count acc accList =
+            match count with
+            | _ when count <= n -> degreeList (count + 1) (float(2) ** float(count)|>int) accList
+            | _ when count > n &&  count <= m + n + 1 -> degreeList (count + 1) (float(2) ** float(count)|>int) (acc::accList)
+            | _ -> List.rev(accList)
+
+        printfn "%A" (degreeList 1 0 [])
+
+else printfn ("error")
