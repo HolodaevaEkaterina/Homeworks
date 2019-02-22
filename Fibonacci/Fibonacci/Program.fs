6e3e459bcef48167aec8704d31823ad2b584e0de
Ekaterina <kata_1998@mail.ru>
2019-02-22 15:18:25 +0300

    Corrected 2 task

--- Fibonacci/Fibonacci/Program.fs
+++ Fibonacci/Fibonacci/Program.fs
@@ -1,16 +1,19 @@
 ﻿let count = System.Console.ReadLine() |> int 
 
 if count > 0 then
- let rec fibonacci n =
-   match n with
-    | 1 | 2 -> 1
-    | _ -> fibonacci (n - 1) + fibonacci (n - 2)
+    let  fibonacci n =
+        let rec fib_helper n acc1 acc2 =
+            match n with
+            | 1 | 2 -> acc2
+            | _ -> fib_helper (n - 1) acc2 (acc1 + acc2)
 
- let rec printFibonacci n  =
-   match n with 
-    | 1 -> printf "%d, " (fibonacci n)
-    | _ -> printFibonacci (n - 1)
-           printf "%d, " (fibonacci n)
+        in fib_helper n 0 1
+
+    let rec printFibonacci n  =
+        match n with 
+            | 1 -> printf "%d, " (fibonacci n)
+            | _ -> printFibonacci (n - 1)
+        printf "%d, " (fibonacci n)
            
- printFibonacci (count)
+    printFibonacci (count)
  else printfn ("n cannot be <=");
