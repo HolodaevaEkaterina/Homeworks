0669da7e93d5c09e4b6123c70cb66a2d9638bb35
Ekaterina <kata_1998@mail.ru>
2019-02-27 11:42:42 +0300

    added 2.1 task

---
+++ PositionInList/PositionInList/Program.fs
@@ -0,0 +1,9 @@
+﻿module PositionInList
+
+let rec firstPosition count element list =
+    match list with
+    | _ when list = [] -> None
+    | _ when List.item count list = element -> Some count
+    | _ when list.Length - 1 = count  -> None 
+    | _ -> firstPosition (count + 1) element list
+printfn "%A" (firstPosition 0 1 [4; 1])
