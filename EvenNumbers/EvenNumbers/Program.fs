783691e35b6db15640cbb3f266c68ccb475b3b12
Ekaterina <kata_1998@mail.ru>
2019-03-03 16:56:55 +0300

    added 3.1 task

---
+++ EvenNumbers/EvenNumbers/Program.fs
@@ -0,0 +1,24 @@
+﻿module EvenNumbers
+
+let evenOnlyWithMap list = 
+    let remainderOfTheDivision = List.map (fun x -> x % 2) list
+    let count = List.length list - List.sum remainderOfTheDivision
+    count
+
+printfn "%A" (evenOnlyWithMap [1; 2; 3; 4; 5])
+
+let evenOnlyWithFilter list = 
+    let evenList = List.filter (fun x -> x % 2 = 0) list
+    let count = List.length evenList
+    count
+
+printfn "%A" (evenOnlyWithFilter [1; 2; 3; 4; 5])
+
+let evenOnlyWithFold list =  
+    let sumOfMod = List.fold ( fun acc x -> acc + x % 2) 0 list
+    let count = List.length list - sumOfMod
+    count   
+
+printfn "%A" (evenOnlyWithFold [])
+
+System.Console.ReadKey()
---
+++ EvenNumbers/EvenNumbersTest/Tests.fs
@@ -0,0 +1,33 @@
+﻿namespace EvenNumbersTest
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit
+
+[<TestClass>]
+type ``Count the number of even numbers ``() =
+
+    [<TestMethod>]
+    member this.``Empty list ``() =
+        EvenNumbers.evenOnlyWithMap [] |> should equal 0
+        EvenNumbers.evenOnlyWithFilter [] |> should equal 0
+        EvenNumbers.evenOnlyWithFold [] |> should equal 0
+
+    [<TestMethod>]
+    member this.`` list without even numbers ``() =
+        EvenNumbers.evenOnlyWithMap [1; 3; 5; 7; 9] |> should equal 0
+        EvenNumbers.evenOnlyWithFilter [1; 3; 5; 7; 9] |> should equal 0
+        EvenNumbers.evenOnlyWithFold [1; 3; 5; 7; 9] |> should equal 0
+
+    [<TestMethod>]
+    member this.`` list with even numbers ``() =
+        EvenNumbers.evenOnlyWithMap [1; 6; 5; 7; 10] |> should equal 2
+        EvenNumbers.evenOnlyWithFilter [1; 6; 5; 7; 10] |> should equal 2
+        EvenNumbers.evenOnlyWithFold [1; 6; 5; 7; 10] |> should equal 2
+
+    [<TestMethod>]
+    member this.``list with even numbers only ``() =
+        EvenNumbers.evenOnlyWithMap [568; 6; 8520; 7414; 92] |> should equal 5
+        EvenNumbers.evenOnlyWithFilter [568; 6; 8520; 7414; 92] |> should equal 5
+        EvenNumbers.evenOnlyWithFold [568; 6; 8520; 7414; 92] |> should equal 5
+
+         
