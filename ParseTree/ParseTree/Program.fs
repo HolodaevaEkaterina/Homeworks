a29d7939a900941c1596ea1017336f951bcb5507
Ekaterina <kata_1998@mail.ru>
2019-03-04 22:28:56 +0300

    Added 3.3 task

---
+++ ParseTree/ParseTree/Program.fs
@@ -0,0 +1,32 @@
+﻿module ParseTree
+
+/// <summary>
+/// arithmetic expression parse tree
+/// </summary>
+
+type Expression =
+    | Plus of Expression * Expression
+    | Minus of Expression * Expression
+    | Multiplication of Expression * Expression
+    | Divide of Expression * Expression
+    | Result of int
+
+/// <summary>
+/// code that computes an arithmetic expression parse tree
+/// </summary>
+
+///<exception cref = System.DivideByZeroException > occurs when dividing by 0 </exception>
+
+let rec expr (p: Expression) =
+    match p with
+    | Result p -> p
+    | Plus(p1, p2) -> expr p1 + expr p2
+    | Minus (p1, p2) -> expr p1 - expr p2
+    | Multiplication (p1, p2) -> expr p1 * expr p2
+    | Divide (p1, p2) -> 
+        try 
+            if (expr p1 % expr p2) = 0 then
+                expr p1 / expr p2
+            else 0
+        with
+        | :? System.DivideByZeroException -> printfn "Division by zero!"; 0
---
+++ ParseTree/ParseTreeTests/Tests.fs
@@ -0,0 +1,62 @@
+﻿namespace ParseTreeTests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit
+
+[<TestClass>]
+type ``ParseTreeTests``() =
+
+    [<TestMethod>]
+    member this.``Test only with addition``() =
+        ParseTree.expr (ParseTree.Plus
+            (ParseTree.Result 5, ParseTree.Plus
+                (ParseTree.Result 4, ParseTree.Result 6))) |> should equal 15
+
+    [<TestMethod>]
+    member this.``Test only with subtraction``() =
+        ParseTree.expr (ParseTree.Minus
+            (ParseTree.Result 254, ParseTree.Minus
+                (ParseTree.Result 10, ParseTree.Result 6))) |> should equal 250
+
+    [<TestMethod>]
+    member this.``Multiplication test only``() =
+        ParseTree.expr (ParseTree.Multiplication
+            (ParseTree.Result 10, ParseTree.Multiplication
+                (ParseTree.Result 7, ParseTree.Result 3))) |> should equal 210
+
+    [<TestMethod>]
+    member this.``Test only with division``() =
+        ParseTree.expr (ParseTree.Divide
+            (ParseTree.Result 1400, ParseTree.Divide
+                (ParseTree.Result 20, ParseTree.Result 10))) |> should equal 700
+
+    [<TestMethod>]
+    member this.``Division test by 0``() =
+        ParseTree.expr (ParseTree.Divide
+            (ParseTree.Result 1400, ParseTree.Divide
+                (ParseTree.Result 20, ParseTree.Result 0))) |> should equal 0
+
+    [<TestMethod>]
+    member this.``Expression with addition and subtraction``() =
+        ParseTree.expr (ParseTree.Plus
+            (ParseTree.Minus
+                 (ParseTree.Result 1400, ParseTree.Plus
+                    (ParseTree.Result 40, ParseTree.Result 60)), ParseTree.Result 200)) |> should equal 1500
+    [<TestMethod>]
+    member this.``Еhe result of the expression is a fraction``() =
+        ParseTree.expr (ParseTree.Divide
+            (ParseTree.Result 1400, ParseTree.Divide
+                (ParseTree.Result 20, ParseTree.Result 13))) |> should equal 0
+       
+    [<TestMethod>]
+    member this.``Expression with the right priority``() =
+        ParseTree.expr (ParseTree.Multiplication
+            (ParseTree.Result 4, ParseTree.Plus
+                (ParseTree.Result 2, ParseTree.Result 3))) |> should equal 20
+
+    [<TestMethod>]
+    member this.``Expression only with multiplication and division``() =
+        ParseTree.expr (ParseTree.Multiplication
+            (ParseTree.Divide
+                 (ParseTree.Result 1400, ParseTree.Divide
+                    (ParseTree.Result 120, ParseTree.Result 60)), ParseTree.Result 20)) |> should equal 14000
