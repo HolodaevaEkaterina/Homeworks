4ca43bd5a05a4b8d9ec738f4f4eae3a5e0674ff1
Ekaterina <kata_1998@mail.ru>
2019-03-07 20:06:12 +0300

    3.4 task

---
+++ SequenceOfPrimeNumbers/SequenceOfPrimeNumbers/Program.fs
@@ -0,0 +1,13 @@
+﻿module PrimeNumbers
+
+let checkIsPrime n =
+    let rec check x =
+        match n with
+        | 0 | 1 -> false
+        | _ when n / 2 < x || (n % x <> 0 && check (x + 1)) -> true
+        | _ -> false
+
+    check 2
+
+let sequenceOfNumbers () = Seq.initInfinite (fun index -> index) |> Seq.filter ( fun x -> checkIsPrime x = true)
+printfn "%A" (sequenceOfNumbers ())
---
+++ SequenceOfPrimeNumbers/SequenceOfPrimeNumbersTests/Tests.fs
@@ -0,0 +1,24 @@
+﻿namespace SequenceOfPrimeNumbersTests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit
+
+[<TestClass>]
+type ``PrimeNumbersTests`` () =
+
+    [<TestMethod>]
+    member this.``The sequence starts with 2``() =
+        PrimeNumbers.sequenceOfNumbers () |> Seq.take 1 |> should equal [2]
+
+    [<TestMethod>]
+    member this.``The sequence contains 997 ``() =
+        PrimeNumbers.sequenceOfNumbers () |> Seq.contains 997 |> should equal true 
+
+    [<TestMethod>]
+    member this.``The sequence of 11, 12, 13 prime numbers ``() =
+        PrimeNumbers.sequenceOfNumbers ()  |> Seq.skip 10 |> Seq.take 3 |> should equal [31; 37; 41]
+
+    [<TestMethod>]
+    member this.``The sequence does not contains 182 ``() =
+        PrimeNumbers.sequenceOfNumbers ()  |> Seq.skip 41 |> Seq.take 2 |> should equal [181; 191]
