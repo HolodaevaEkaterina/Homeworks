addd3c817bbe56f237a4d03b2ecabd253bc58397
Ekaterina <kata_1998@mail.ru>
2019-03-27 11:30:47 +0300

    5.1 Task

---
+++ SequenceOfBrackets/SequenceOfBrackets/Program.fs
@@ -0,0 +1,17 @@
+﻿  module SequenceOfBrackets
+
+  let rec checkBraskets (ourString : string) acc count1 count2 count3 =
+
+    match ourString with
+    |_  when ourString.Length = acc -> if count1 = 0 && count2 = 0 && count3 = 0 then Some true
+                                       else Some false
+    |_  when ourString.[acc] = '(' -> checkBraskets ourString (acc + 1) (count1 + 1) count2 count3
+    |_  when ourString.[acc] = ')' -> if count1 <= 0  then Some false
+                                      else checkBraskets ourString (acc + 1) (count1 - 1) count2 count3   
+    |_  when ourString.[acc] = '{' -> checkBraskets ourString (acc + 1) count1 (count2 + 1) count3
+    |_  when ourString.[acc] = '}' -> if count2 <= 0  then Some false
+                                      else checkBraskets ourString (acc + 1) count1 (count2 - 1) count3  
+    |_  when ourString.[acc] = '[' -> checkBraskets ourString (acc + 1) count1 count2 (count3 + 1)
+    |_  when ourString.[acc] = ']' -> if count3 <= 0  then Some false
+                                      else checkBraskets ourString (acc + 1) count1 count2 (count3 - 1)  
+    |_ -> checkBraskets ourString (acc + 1) count1  count2 count3   
