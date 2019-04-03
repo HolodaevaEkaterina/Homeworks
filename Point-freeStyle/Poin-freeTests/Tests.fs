6639985686253e8d1f8687b7012beea91e09b748
Ekaterina <kata_1998@mail.ru>
2019-04-04 00:22:44 +0300

    added 5.2 task

---
+++ Point-freeStyle/Poin-freeTests/Tests.fs
@@ -0,0 +1,8 @@
+﻿module Point_freeTests
+
+open NUnit.Framework
+open FsCheck
+
+[<Test>]
+let ``Let us prove that the poin-free function gives the same result``() =
+        Check.Quick(fun x l -> (PointFreeStyle.func x l) = (PointFreeStyle.func3 x l)) 
