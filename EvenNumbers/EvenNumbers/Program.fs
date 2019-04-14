d0477d9ced2659fcba925ffeec63b26e801fabe8
Ekaterina <kata_1998@mail.ru>
2019-04-14 18:13:48 +0300

    Corrections according to requirements

--- EvenNumbers/EvenNumbers/Program.fs
+++ EvenNumbers/EvenNumbers/Program.fs
@@ -1,24 +1,12 @@
 ﻿module EvenNumbers
 
 let evenOnlyWithMap list = 
-    let remainderOfTheDivision = List.map (fun x -> x % 2) list
-    let count = List.length list - List.sum remainderOfTheDivision
+    let count = List.map (fun x -> if (x % 2) = 0 then 1 else 0) list |> List.sum    
     count
-
-printfn "%A" (evenOnlyWithMap [1; 2; 3; 4; 5])
 
 let evenOnlyWithFilter list = 
     let evenList = List.filter (fun x -> x % 2 = 0) list
     let count = List.length evenList
     count
 
-printfn "%A" (evenOnlyWithFilter [1; 2; 3; 4; 5])
-
-let evenOnlyWithFold list =  
-    let sumOfMod = List.fold ( fun acc x -> acc + x % 2) 0 list
-    let count = List.length list - sumOfMod
-    count   
-
-printfn "%A" (evenOnlyWithFold [])
-
-System.Console.ReadKey()
+let evenOnlyWithFold list =  List.fold ( fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0 list
--- EvenNumbers/EvenNumbersTest/Tests.fs
+++ EvenNumbers/EvenNumbersTest/Tests.fs
@@ -1,33 +1,38 @@
 ﻿namespace EvenNumbersTest
 
-open Microsoft.VisualStudio.TestTools.UnitTesting
-open FsUnit
+module Tests = 
+    open FsUnit
+    open FsCheck
+    open EvenNumbers
+    open NUnit.Framework
+   
+    let list1 = []
+    let list2 = [1; 3; 5; 7; 9]
+    let list3 = [1; 6; 5; 7; 10]
+    let list4 = [568; 6; 8520; 7414; 92]
 
-[<TestClass>]
-type ``Count the number of even numbers ``() =
 
-    [<TestMethod>]
-    member this.``Empty list ``() =
-        EvenNumbers.evenOnlyWithMap [] |> should equal 0
-        EvenNumbers.evenOnlyWithFilter [] |> should equal 0
-        EvenNumbers.evenOnlyWithFold [] |> should equal 0
+    [<Test>]
+    let``Empty list ``() =
+        Check.Quick(evenOnlyWithMap list1 = evenOnlyWithFilter list1) 
+        Check.Quick(evenOnlyWithMap list1 = evenOnlyWithFold list1)
+        evenOnlyWithFold list1 |> should equal 0
 
-    [<TestMethod>]
-    member this.`` list without even numbers ``() =
-        EvenNumbers.evenOnlyWithMap [1; 3; 5; 7; 9] |> should equal 0
-        EvenNumbers.evenOnlyWithFilter [1; 3; 5; 7; 9] |> should equal 0
-        EvenNumbers.evenOnlyWithFold [1; 3; 5; 7; 9] |> should equal 0
+    [<Test>]
+    let`` list without even numbers ``() =
+        Check.Quick(evenOnlyWithMap list2 = evenOnlyWithFilter list2) 
+        Check.Quick(evenOnlyWithMap list2 = evenOnlyWithFold list2)
+        evenOnlyWithFold list2 |> should equal 0
 
-    [<TestMethod>]
-    member this.`` list with even numbers ``() =
-        EvenNumbers.evenOnlyWithMap [1; 6; 5; 7; 10] |> should equal 2
-        EvenNumbers.evenOnlyWithFilter [1; 6; 5; 7; 10] |> should equal 2
-        EvenNumbers.evenOnlyWithFold [1; 6; 5; 7; 10] |> should equal 2
+    [<Test>]
+    let`` list with even numbers ``() =
+        Check.Quick(evenOnlyWithMap list3 = evenOnlyWithFilter list3) 
+        Check.Quick(evenOnlyWithMap list3 = evenOnlyWithFold list3)
+        evenOnlyWithFold list3 |> should equal 2
 
-    [<TestMethod>]
-    member this.``list with even numbers only ``() =
-        EvenNumbers.evenOnlyWithMap [568; 6; 8520; 7414; 92] |> should equal 5
-        EvenNumbers.evenOnlyWithFilter [568; 6; 8520; 7414; 92] |> should equal 5
-        EvenNumbers.evenOnlyWithFold [568; 6; 8520; 7414; 92] |> should equal 5
-
+    [<Test>]
+    let``list with even numbers only ``() =
+        Check.Quick(evenOnlyWithMap list4 = evenOnlyWithFilter list4) 
+        Check.Quick(evenOnlyWithMap list4 = evenOnlyWithFold list4)
+        evenOnlyWithFold list4 |> should equal 5
          
