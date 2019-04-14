662b6e29cb6e32415eabbba65ce78a7ed5832b42
Ekaterina <kata_1998@mail.ru>
2019-04-14 22:22:17 +0300

    7.2 task

---
+++ ImplementationWorkflow2/ImplementationWorkflow2/Program.fs
@@ -0,0 +1,18 @@
+﻿module ImplementationWorkflow
+
+open System
+
+///checking the correctness of the entered lines
+let validationCheck(str: string) = 
+    match Int32.TryParse(str) with
+    | (true,int) -> Some(int)
+    | _ -> None
+ 
+ ///performing calculations
+type CalculationBuilder() =
+    member this.Bind(x: string, f) =
+        let value = validationCheck(x)
+        match value with
+        | None -> None
+        | Some a -> f a
+    member this.Return(x) = Some x
