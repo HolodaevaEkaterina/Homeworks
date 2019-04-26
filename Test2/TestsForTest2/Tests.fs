4636069d7e24ac7b7c0557bd0ff52870d4c7be52
Ekaterina <kata_1998@mail.ru>
2019-04-26 19:01:29 +0300

    Test2

---
+++ Test2/TestsForTest2/Tests.fs
@@ -0,0 +1,13 @@
+﻿namespace TestsForTest2
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit
+
+[<TestClass>]
+type ``TestsForTest2`` () =
+
+    [<TestMethod>]
+    member this.``The function should return "906609" ``() =
+        Palindrome.searchOptions 998001 |> should equal "906609"
+        
