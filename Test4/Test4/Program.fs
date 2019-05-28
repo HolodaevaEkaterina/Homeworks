8d9400bf9526817e71d999b3029c8bfaf089cbdd
Ekaterina <kata_1998@mail.ru>
2019-05-28 16:16:45 +0300

    Test try 4

---
+++ Test4/Test4/Program.fs
@@ -0,0 +1,25 @@
+﻿module Test4
+
+type Auto =
+    | Car of Model : string * Colour : string * Brand : string * Price : int * Sold : bool
+    | Track of Brand : string * Price : int * Sold : bool * Weight : int
+
+let rec  brandAuto (auto : Auto) = 
+    match auto with
+    |Car (model, colour, brand, price, true) -> brand 
+    |Car (model, colour, brand, price, false) -> ""
+    |Track (brand, price, true, weight) ->  brand 
+    |Track (brand, price, false, weight) -> ""
+
+let rec  sumAuto (auto : Auto) = 
+    match auto with
+    |Car (model, colour, brand, price, true) ->   price
+    |Car (model, colour, brand, price, false) -> 0
+    |Track (brand, price, true, weight) ->  price
+    |Track (brand, price, false, weight) -> 0
+
+let brands (list : Auto list) = List.filter(fun x -> x <> "") (List.distinct(List.map(fun x -> (brandAuto x)) list))
+let sum (list : Auto list) = List.sum(List.map(fun x -> (sumAuto x)) list)
+
+
+
---
+++ Test4/TestsForTest4/Tests.fs
@@ -0,0 +1,28 @@
+﻿namespace TestsForTest4
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit
+open Test4
+
+[<TestClass>]
+type ``Tests for test4`` () =
+
+    let salon : Auto list  = [Car("a","b","c", 1, true); Track("a", 1, true, 2)]
+    let salon1 : Auto list  = [Car("a","b","c",100, true); Track("c", 50, true, 2)]
+    let salon2 : Auto list  = [Car("a", "b", "c", 100, false); Car("C4","Black","Volga",2000, true)]
+
+    [<TestMethod>]
+    member this.``Random test `` () =
+        brands salon |> should equal ["c"; "a"]
+        sum salon |> should equal 2
+
+    [<TestMethod>]
+    member this.``Duplicate test `` () =
+        brands salon1 |> should equal ["c"]
+        sum salon1 |> should equal 150
+
+    [<TestMethod>]
+    member this.``Not all cars are sold`` () =
+        brands salon2 |> should equal ["Volga"]
+        sum salon2 |> should equal 2000
+        
