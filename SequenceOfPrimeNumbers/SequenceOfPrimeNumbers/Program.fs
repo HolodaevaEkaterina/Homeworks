9955b62a93c8d57797026ab92ff639420dd9cfa8
Ekaterina <kata_1998@mail.ru>
2019-06-02 00:50:57 +0300

    notes corrected

--- SequenceOfPrimeNumbers/SequenceOfPrimeNumbers/Program.fs
+++ SequenceOfPrimeNumbers/SequenceOfPrimeNumbers/Program.fs
@@ -1,13 +1,14 @@
 ﻿module PrimeNumbers
 
+///finds the remainder of dividing by all numbers less than the square root
 let checkIsPrime n =
     let rec check x =
         match n with
         | 0 | 1 -> false
-        | _ when n / 2 < x || (n % x <> 0 && check (x + 1)) -> true
+        | _ when sqrt(n |> float) |> int < x || (n % x <> 0 && check (x + 1)) -> true
         | _ -> false
 
     check 2
 
-let sequenceOfNumbers () = Seq.initInfinite (fun index -> index) |> Seq.filter ( fun x -> checkIsPrime x = true)
+let sequenceOfNumbers () = Seq.initInfinite (id) |> Seq.filter ( fun x -> checkIsPrime x)
 printfn "%A" (sequenceOfNumbers ())
