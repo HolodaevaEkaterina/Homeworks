a0498123a9f98c861fa87b26fb2268415178c271
Ekaterina <kata_1998@mail.ru>
2019-06-06 12:24:56 +0300

    8.1 task

---
+++ LazyCalculation/LazyCalculation/Program.fs
@@ -0,0 +1,62 @@
+﻿module LazyCalculation =
+    open System.Threading
+
+    type ILazy<'a> =
+        abstract member Get: unit -> 'a
+
+    type SingleLazy<'a> (supplier : unit -> 'a) = 
+        
+        let mutable var = None
+
+        interface ILazy<'a> with
+            member this.Get () = 
+                match var with 
+                | Some x -> x
+                | None -> 
+                    let res = supplier ()
+                    var <- Some res
+                    res
+
+    type MultiLazy<'a> (supplier : unit -> 'a) = 
+
+        let protect = obj()
+        let mutable var = None
+        
+        interface ILazy<'a> with 
+            member this.Get () = 
+                Monitor.Enter protect
+                try
+                    match var with
+                    | Some x -> x
+                    | None ->
+                        let res = supplier ()
+                        var <- Some res
+                        Option.get var
+                finally
+                    Monitor.Exit protect                  
+                
+    type LockFreeLazy<'a> (supplier : unit -> 'a) = 
+
+        let mutable var = None
+
+        interface ILazy<'a> with
+            member this.Get () = 
+                match var with
+                | Some x -> x
+                | None ->
+                    let res = supplier ()
+                    Interlocked.CompareExchange(&var, Some res, None) |> ignore
+                    Option.get var
+
+open LazyCalculation
+
+module LazyFactory =
+
+    type LazyFactory() =
+         
+        static member CreateSingleThreadedLazy (supplier : unit -> 'a) = 
+            SingleLazy<'a>(supplier)
+        static member CreateMultiThreadedLazy (supplier : unit -> 'a) = 
+            MultiLazy<'a>(supplier) 
+        static member CreateLockFreeThreadedLazy (supplier : unit -> 'a) = 
+            LockFreeLazy<'a>(supplier)
---
+++ LazyCalculation/TestsForLazyCalculation/Tests.fs
@@ -0,0 +1,44 @@
+﻿namespace TestsForLazyCalculation
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Program
+open LazyFactory
+open LazyCalculation
+open FsUnit
+open System
+open System.Threading
+
+[<TestClass>]
+type ``Tests for Lazy Calculation`` () =
+    let random = Random()
+
+    [<TestMethod>]
+    member this.``Test for single threaded mode`` () =
+        let list = [1..10]
+        let obj = LazyFactory.CreateSingleThreadedLazy(fun () -> random.Next())
+        let actualList = List.map(fun x -> (obj :> ILazy<int>).Get()) list
+        let expectedResult = (obj :> ILazy<int>).Get()
+        actualList |> List.iter (should equal expectedResult)
+
+    [<TestMethod>]
+    member this.``Test for multithreaded mode`` () =
+        
+        let object = LazyFactory.CreateMultiThreadedLazy(fun () -> random.Next())
+        let threadCount = 80
+        let results = Array.zeroCreate threadCount
+        let threads = [| for index in 0..threadCount - 1 -> new Thread(fun () -> results.[index] <-  (object :> ILazy<int>).Get()) |]
+        threads |> Array.iter (fun thread -> thread.Start())
+        threads |> Array.iter (fun thread -> thread.Join())
+        let expectedResult = (object :> ILazy<int>).Get()
+        results |> Array.iter (should equal expectedResult)
+
+    [<TestMethod>]
+    member this.``Test for lock-free`` () =
+        let object = LazyFactory.CreateLockFreeThreadedLazy(fun () -> random.Next())
+        let threadCount = 80
+        let results = Array.zeroCreate threadCount
+        let threads = [| for index in 0..threadCount - 1 -> new Thread(fun () -> results.[index] <- (object :> ILazy<int>).Get()) |]
+        threads |> Array.iter (fun thread -> thread.Start())
+        threads |> Array.iter (fun thread -> thread.Join())
+        let expectedResult = (object :> ILazy<int>).Get()
+        results |> Array.iter (should equal expectedResult)
