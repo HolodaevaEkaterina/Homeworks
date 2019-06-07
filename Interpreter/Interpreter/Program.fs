20128973fd6bdcfc1a67cc9f03fb2bbb98154bda
Ekaterina <kata_1998@mail.ru>
2019-06-07 23:29:16 +0300

    notes corrected

--- Interpreter/Interpreter/Program.fs
+++ Interpreter/Interpreter/Program.fs
@@ -1,35 +1,58 @@
 ﻿module Interpreter
 
-type term = 
-    |Variable of char
-    |Application of  term * term
-    |LambdaAbstraction of char * term 
+type Var = string
 
-let subst substedVar substing inTerm =
-    let rec substitution substedVar substing expr list =
-        match expr with
-        | Variable (var) -> if List.exists (fun x -> x = substedVar) list <> true && var = substedVar then substing else expr
-        | Application (expr1, expr2) -> Application (substitution substedVar substing expr1 list, substitution substedVar substing expr2 list)
-        | LambdaAbstraction (var, expr) -> LambdaAbstraction (var, substitution substedVar substing expr (var::list))
-    substitution substedVar substing inTerm []
-      
-let rec reduceOnce expr =
-    match expr with
-    | Variable _ -> None
-    | Application (LambdaAbstraction (var, expr1), expr2) -> Some (subst var expr2 expr1)
-    | Application (expr1, expr2) ->
-        match reduceOnce expr1 with
-        | Some x -> Some (Application (x, expr2))
-        | _ -> 
-        match reduceOnce expr2 with
-        | Some x -> Some (Application (expr1, x))
-        | _ -> None
-    | LambdaAbstraction (var, expr) ->
-        match reduceOnce expr with
-        | Some x -> Some (LambdaAbstraction (var, x))
-        | _ -> None
+type Term = 
+    | Variable of string
+    | Application of  Term * Term
+    | LambdaAbstraction of string * Term 
 
-let rec reduce expr =
-    match reduceOnce expr with
-    | Some x -> reduce x
-    | _ -> expr
+///beta-reduction
+let rec reduction (term:Term) (var : Var) value =
+    match term with
+    | Variable(w) when w = var -> value
+    | Application (x, y) -> Application(reduction x var value, reduction y var value)
+    | LambdaAbstraction (x, y) -> LambdaAbstraction (x, reduction y var value)
+    | _ -> term
+
+let newVar (x : string) = (x + "1")
+
+/// renaming
+let rec rename term (v:Var) =
+    match term with
+    | Variable x  when x = v -> Variable (newVar x)
+    | Application(x, y) -> Application(rename x v, rename y v)
+    | LambdaAbstraction (x, y) -> match x with
+                                  | x when x = v -> LambdaAbstraction (newVar x, rename y v)
+                                  | _ -> LambdaAbstraction (x, rename y v)
+    | _ -> term
+
+/// lambda abstraction check
+let rec containAbstraction term =
+        match term with
+        | Variable(_) -> false
+        | LambdaAbstraction (y, z) -> true
+        | Application(y, z) -> (containAbstraction y) || (containAbstraction z)
+
+/// entry check        
+let rec withSameLetter term l =
+    match term with
+    | x when x = l -> true
+    | LambdaAbstraction (y, z) -> (Variable y = l) || (withSameLetter z l)
+    | Application(y, z) -> (withSameLetter y l) || (withSameLetter z l)
+    | _ -> false
+
+let rec reduce term =
+    match term with
+    | Variable (_) -> term
+    | Application (x, y) -> 
+        match x with 
+        | LambdaAbstraction (t, u) -> match y with
+                       | Variable(v) -> if (withSameLetter x y) then (reduce <| reduction (rename u v) (if (t = v) then (newVar t) else (t)) y) else (reduce <| reduction u t y)
+                       | _ -> reduce <| reduction u t y
+        | Variable(_) -> Application(x, y)
+        | _ -> if (containAbstraction term) then reduce <| Application(reduce x, reduce y) else Application(reduce x, reduce y)
+    | LambdaAbstraction (x, y) -> 
+        match y with
+        | Variable (_) -> term
+        | _ -> LambdaAbstraction (x, reduce y)
--- Interpreter/InterpreterTests/Tests.fs
+++ Interpreter/InterpreterTests/Tests.fs
@@ -11,9 +11,14 @@ type ``InterpreterTests ``() =
 
     [<TestMethod>]
     member this.`` Test for simple expression``() =
-         reduce (Application(LambdaAbstraction ('x', Variable 'x'), Variable 'x')) |> should equal (Variable 'x')
+         reduce (Application(LambdaAbstraction ("x", Variable "x"), Variable "x")) |> should equal (Variable "x")
     
     [<TestMethod>]
     member this.`` Test for random expression``() =
-         let expr = LambdaAbstraction('x',Application(Application(Variable 'x', Variable 'x'), Variable 'x'))
-         reduce (Application(LambdaAbstraction('x', Variable 'y'), Application(expr, expr)))|> should equal (Variable 'y')
+         let expr = LambdaAbstraction("x",Application(Application(Variable "x", Variable "x"), Variable "x"))
+         reduce (Application(LambdaAbstraction("x", Variable "y"), Application(expr, expr)))|> should equal (Variable "y")
+     
+    [<TestMethod>]
+    member this.`` Test with renaming``() =
+         let expr = LambdaAbstraction("x",Application(Application(Variable "x", Variable "x"), Variable "x"))
+         reduce (Application(LambdaAbstraction("x", LambdaAbstraction("y", Application(Variable "x", Variable "y"))), Variable "y")) |> should equal (LambdaAbstraction( "y1", Application( Variable "y" , Variable "y1" )))
