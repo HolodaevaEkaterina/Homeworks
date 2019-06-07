549a444a913e100337854448c9ec96ca5511644f
Ekaterina <kata_1998@mail.ru>
2019-06-08 01:31:31 +0300

    new fixes

--- Interpreter/Interpreter/Program.fs
+++ Interpreter/Interpreter/Program.fs
@@ -1,58 +1,51 @@
 ﻿module Interpreter
 
+open System
+
+/// variable definition
 type Var = string
 
+/// term definition
 type Term = 
-    | Variable of string
+    | Variable of Var
     | Application of  Term * Term
-    | LambdaAbstraction of string * Term 
+    | LambdaAbstraction of Var * Term 
 
-///beta-reduction
-let rec reduction (term:Term) (var : Var) value =
-    match term with
-    | Variable(w) when w = var -> value
-    | Application (x, y) -> Application(reduction x var value, reduction y var value)
-    | LambdaAbstraction (x, y) -> LambdaAbstraction (x, reduction y var value)
-    | _ -> term
-
-let newVar (x : string) = (x + "1")
+/// find free variables
+let getFreeVariable (term : Term) = 
+    let rec freeVar (term' : Term) (list : string list) = 
+        match term' with
+        | Variable (x) -> x :: list
+        | Application (term1, term2) -> (freeVar term1 list) @ (freeVar term2 list)
+        | LambdaAbstraction (var, term3) -> freeVar term3 list |> List.filter ((<>) var)
+    freeVar term [] |> List.distinct
 
 /// renaming
-let rec rename term (v:Var) =
+let namesVar () = ['a' .. 'w'] |> List.map Char.ToString
+let newName (busy : string list) = 
+    let rec generate (iter : int) = 
+        let names = namesVar () |> List.map (String.replicate iter)
+        let notBusy = ((Set.ofList names) - (Set.ofList busy)) |> Set.toList
+        match notBusy.Length with
+        | 0 -> generate (iter + 1)
+        | _ -> List.head notBusy
+    generate 1
+
+/// substitution
+let rec reduction (term:Term) (variable : Var) value =
     match term with
-    | Variable x  when x = v -> Variable (newVar x)
-    | Application(x, y) -> Application(rename x v, rename y v)
-    | LambdaAbstraction (x, y) -> match x with
-                                  | x when x = v -> LambdaAbstraction (newVar x, rename y v)
-                                  | _ -> LambdaAbstraction (x, rename y v)
-    | _ -> term
+    | Variable(v) when v = variable -> value
+    | Variable(v) -> term
+    | Application (term1, term2) -> Application(reduction term1 variable value, reduction term2 variable value)
+    | LambdaAbstraction (var, term1) when var = variable -> term
+    | LambdaAbstraction (var, term1) when not (getFreeVariable value |> List.contains var) || not (getFreeVariable term |> List.contains variable) -> LambdaAbstraction(var, reduction term1 variable value)
+    | LambdaAbstraction (var, term1) ->  let str = newName ((getFreeVariable term) @ (getFreeVariable value))
+                                         LambdaAbstraction(str, reduction (reduction term1 var (Variable str)) variable value) 
 
-/// lambda abstraction check
-let rec containAbstraction term =
-        match term with
-        | Variable(_) -> false
-        | LambdaAbstraction (y, z) -> true
-        | Application(y, z) -> (containAbstraction y) || (containAbstraction z)
-
-/// entry check        
-let rec withSameLetter term l =
-    match term with
-    | x when x = l -> true
-    | LambdaAbstraction (y, z) -> (Variable y = l) || (withSameLetter z l)
-    | Application(y, z) -> (withSameLetter y l) || (withSameLetter z l)
-    | _ -> false
-
+/// reduction
 let rec reduce term =
     match term with
     | Variable (_) -> term
-    | Application (x, y) -> 
-        match x with 
-        | LambdaAbstraction (t, u) -> match y with
-                       | Variable(v) -> if (withSameLetter x y) then (reduce <| reduction (rename u v) (if (t = v) then (newVar t) else (t)) y) else (reduce <| reduction u t y)
-                       | _ -> reduce <| reduction u t y
-        | Variable(_) -> Application(x, y)
-        | _ -> if (containAbstraction term) then reduce <| Application(reduce x, reduce y) else Application(reduce x, reduce y)
-    | LambdaAbstraction (x, y) -> 
-        match y with
-        | Variable (_) -> term
-        | _ -> LambdaAbstraction (x, reduce y)
+    | Application (LambdaAbstraction(var, term), term2) -> reduce (reduction term var term2)
+    | Application(term1, term2) -> reduce (Application(term1, term2))
+    | LambdaAbstraction (var, term) -> LambdaAbstraction (var, reduce term)
--- Interpreter/InterpreterTests/Tests.fs
+++ Interpreter/InterpreterTests/Tests.fs
@@ -9,6 +9,14 @@ open Interpreter
 [<TestClass>]
 type ``InterpreterTests ``() =
 
+    let term1 = LambdaAbstraction("x", Variable("x"))
+    let var = "x"
+    let term2 = Variable("y")
+
+    let term3 = LambdaAbstraction("x", Application(Variable("x"), Variable("y")))
+    let var2 = "y"
+    let term4 = Variable("z")
+
     [<TestMethod>]
     member this.`` Test for simple expression``() =
          reduce (Application(LambdaAbstraction ("x", Variable "x"), Variable "x")) |> should equal (Variable "x")
@@ -18,7 +26,15 @@ type ``InterpreterTests ``() =
          let expr = LambdaAbstraction("x",Application(Application(Variable "x", Variable "x"), Variable "x"))
          reduce (Application(LambdaAbstraction("x", Variable "y"), Application(expr, expr)))|> should equal (Variable "y")
      
+    
     [<TestMethod>]
-    member this.`` Test with renaming``() =
-         let expr = LambdaAbstraction("x",Application(Application(Variable "x", Variable "x"), Variable "x"))
-         reduce (Application(LambdaAbstraction("x", LambdaAbstraction("y", Application(Variable "x", Variable "y"))), Variable "y")) |> should equal (LambdaAbstraction( "y1", Application( Variable "y" , Variable "y1" )))
+    member this.``Test with substitution``() =
+         reduction term1 var term2 |> should equal (LambdaAbstraction("x", Variable("x")))
+
+    [<TestMethod>]
+    member this.``Another test with substitution``() =
+        reduction term3 var2 term4 |> should equal (LambdaAbstraction("x", Application(Variable("x"), Variable("z"))))
+
+    [<TestMethod>]
+    member this.``Test with free variables``() =
+         getFreeVariable term3 |> should equal ["y"]
