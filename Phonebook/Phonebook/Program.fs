530a80386866c2eed35718710717ba4f5e2d55b3
Ekaterina <kata_1998@mail.ru>
2019-06-07 19:15:40 +0300

    tests added

--- Phonebook/Phonebook/Program.fs
+++ Phonebook/Phonebook/Program.fs
@@ -13,6 +13,33 @@ let showOpportunities =
     printfn "6. сохранить текущие данные в файл"
     printfn "7. считать данные из файла"
 
+let addNote number name phonebook = Map.add number name phonebook
+let findPhone phonebook desiredName = phonebook |> Map.filter(fun key value -> value = desiredName)
+let findName phonebook desiredNumber =  phonebook |> Map.find desiredNumber
+let saveToFile fileName phonebook = let fs = new FileStream(fileName, FileMode.Create, FileAccess.Write)
+                                    use writer = new StreamWriter(fs)
+                                    phonebook |> Map.toList |> List.fold (fun acc pair -> acc + (snd pair) + " " + (fst pair) + writer.NewLine) "" |> writer.Write 
+                                    writer.Close()
+                                    
+let readData (sr : StreamReader) = 
+        let rec readData' (sr : StreamReader) phoneBook = 
+            if (not sr.EndOfStream)
+            then
+                let subString = sr.ReadLine()
+                readData' sr (subString :: phoneBook)
+            else
+                sr.Close()
+                phoneBook
+        readData' sr []
+
+let readFromFile (filePath : string) = 
+    if (File.Exists filePath)
+    then
+        use sr = new StreamReader(File.OpenRead(filePath))
+        readData sr
+    else
+            []
+
 let rec furtherAction phonebook =
     let userChoice = Console.ReadLine()
     match userChoice with
@@ -22,16 +49,16 @@ let rec furtherAction phonebook =
         let name = Console.ReadLine()
         printfn "Enter new number"
         let number = Console.ReadLine()
-        furtherAction <| Map.add number name phonebook
+        furtherAction <| addNote number name phonebook
     | "3" ->
         printfn "Enter name"
         let desiredName = Console.ReadLine()
-        phonebook |> Map.filter(fun key value -> value = desiredName) |> Map.iter (fun key value -> printfn "number: %s name: %s" key value)
+        printfn "%A" (findPhone phonebook desiredName |> Map.iter (fun key value -> printfn "number: %s name: %s" key value))
         furtherAction phonebook
     | "4" ->
         printfn "Enter new number"
         let desiredNumber = Console.ReadLine()
-        phonebook |> Map.find desiredNumber |> printf "%A"
+        findName phonebook desiredNumber |> printf "%A"
         furtherAction phonebook
     | "5" -> 
         phonebook |> Map.iter (fun key value -> printfn "number: %s name: %s" key value)
@@ -39,26 +66,15 @@ let rec furtherAction phonebook =
     | "6" ->
         printf "Enter name of file: "
         let fileName = Console.ReadLine() 
-        let fs = new FileStream(fileName, FileMode.Create, FileAccess.Write)
-        use writer = new StreamWriter(fs)
-        phonebook |> Map.toList |> List.fold (fun acc pair -> acc + (snd pair) + " " + (fst pair) + writer.NewLine) "" |> writer.Write 
-        writer.Close()
+        saveToFile fileName phonebook
         printfn "Done"
         furtherAction phonebook
     | "7" ->
-        try
-        printf "Enter name of file: "
-        let fileName = System.Console.ReadLine() 
-        use stream = new StreamReader(fileName)
-
-        let mutable valid = true
-        while (valid) do
-            let line = stream.ReadLine()
-            if (line = null) then
-                valid <- false
-            else
-                printfn "%A" line
-        with :? FileNotFoundException -> printfn "File not found"
-        furtherAction phonebook
+       try
+           printf "Enter name of file: "
+           let fileName = System.Console.ReadLine() 
+           printfn "%A" (readFromFile fileName)
+           furtherAction phonebook
+       with :? FileNotFoundException -> printfn "File not found"
     | _ -> furtherAction phonebook
 furtherAction (Map.ofList [])
