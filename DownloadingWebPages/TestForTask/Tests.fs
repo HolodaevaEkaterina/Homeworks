6d81774a7467e3e000cf387f501f0ff67023eef0
Ekaterina <kata_1998@mail.ru>
2019-06-09 00:14:06 +0300

    not all files were uploaded

---
+++ DownloadingWebPages/TestForTask/Tests.fs
@@ -0,0 +1,13 @@
+﻿namespace TestForTask
+
+open FsUnit
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open DownloadingWebPages
+
+[<TestClass>]
+type ``TestForTask `` () =
+
+    [<TestMethod>]
+    member this.``Test for unlinked page `` () =
+        let pages = List.length(webPageLoading "http://se.math.spbu.ru")
+        pages |> should equal 1
