df45e59771e89a6ef74eff0013e62852db71ab57
Ekaterina <kata_1998@mail.ru>
2019-06-08 11:41:45 +0300

    task redone

--- SequenceOfBrackets/BracketsTests/Tests.fs
+++ SequenceOfBrackets/BracketsTests/Tests.fs
@@ -8,40 +8,44 @@ type ``BracketsTests`` () =
 
     [<TestMethod>]
     member this.``String only with '(', ')' brackets`` () =
-        SequenceOfBrackets.checkBraсkets "5+2*(2+3)" 0 0 0 0 |> should equal (Some true)
+        SequenceOfBrackets.checkBraсkets "5+2*(2+3)" |> should equal true
     
     [<TestMethod>]
     member this.``Incorrect string only with '(', ')' brackets`` () =
-        SequenceOfBrackets.checkBraсkets "5+2*(2)+3)" 0 0 0 0 |> should equal (Some false)
+        SequenceOfBrackets.checkBraсkets "5+2*(2)+3)" |> should equal false
     
     [<TestMethod>]
     member this.``String only with ']', '[' brackets`` () =
-        SequenceOfBrackets.checkBraсkets "a[x] = 25" 0 0 0 0 |> should equal (Some true)
+        SequenceOfBrackets.checkBraсkets "a[x] = 25" |> should equal true
     
     [<TestMethod>]
     member this.``Incorrect string only with ']', '[' brackets`` () =
-        SequenceOfBrackets.checkBraсkets "a[x] = 2]5" 0 0 0 0 |> should equal (Some false)
+        SequenceOfBrackets.checkBraсkets "a[x] = 2]5" |> should equal false
 
     [<TestMethod>]
     member this.``String only with '{', '}' brackets`` () =
-        SequenceOfBrackets.checkBraсkets "x{1,25,y} = |69|" 0 0 0 0 |> should equal (Some true)
+        SequenceOfBrackets.checkBraсkets "x{1,25,y} = |69|" |> should equal true
     
     [<TestMethod>]
     member this.``Incorrect string only with '{', '}' brackets`` () =
-        SequenceOfBrackets.checkBraсkets "{x = {y,z,x}" 0 0 0 0 |> should equal (Some false)
+        SequenceOfBrackets.checkBraсkets "{x = {y,z,x}" |> should equal false
     
     [<TestMethod>]
     member this.``Correct string`` () =
-        SequenceOfBrackets.checkBraсkets "{5 + [7 - 12 : (8 - 5) : 3] + 7 - 2} :[3 + 6 : (5 - 1)]" 0 0 0 0 |> should equal (Some true)
+        SequenceOfBrackets.checkBraсkets "{5 + [7 - 12 : (8 - 5) : 3] + 7 - 2} :[3 + 6 : (5 - 1)]" |> should equal true
     
     [<TestMethod>]
     member this.``Incorrect string`` () =
-        SequenceOfBrackets.checkBraсkets "{5 + [7 - 12 : (8 - 5) : 3] + 7) - 2} :[3 + 6 : (5 - 1)]" 0 0 0 0 |> should equal (Some false)
+        SequenceOfBrackets.checkBraсkets "{5 + [7 - 12 : (8 - 5) : 3] + 7) - 2} :[3 + 6 : (5 - 1)]" |> should equal false
 
     [<TestMethod>]
     member this.``Empty string`` () =
-        SequenceOfBrackets.checkBraсkets "" 0 0 0 0 |> should equal (None)
+        SequenceOfBrackets.checkBraсkets "" |> should equal true
 
     [<TestMethod>]
     member this.``String without brackets`` () =
-        SequenceOfBrackets.checkBraсkets "a*45 /  69 x + 87ab" 0 0 0 0 |> should equal (Some true)
+        SequenceOfBrackets.checkBraсkets "a*45 /  69 x + 87ab" |> should equal true
+    
+    [<TestMethod>]
+    member this.``Overlapping brackets shall not be allowed`` () =
+        SequenceOfBrackets.checkBraсkets "([)]" |> should equal false
--- SequenceOfBrackets/SequenceOfBrackets/Program.fs
+++ SequenceOfBrackets/SequenceOfBrackets/Program.fs
@@ -1,20 +1,21 @@
 ﻿  module SequenceOfBrackets
 
-  let funHelper bracket = bracket <= 0
-      
-  let rec checkBraсkets (ourString : string) helper round square figured =
+  let checkBraсkets myString =
+      let rec helper myString stack =
+          match (myString, stack) with
+          | (('(' :: st), stack) -> helper st ('(' :: stack)
+          | (('[' :: st), stack) -> helper st ('[' :: stack)
+          | (('{' :: st), stack) -> helper st ('{' :: stack)
+          | ((')' :: st), ('(' :: stac)) -> helper st stac
+          | ((']' :: st), ('[' :: stac)) -> helper st stac
+          | (('}' :: st), ('{' :: stac)) -> helper st stac
+          | ([], []) -> true
+          | ([], _) -> false
+          | ((')' :: _), _) -> false
+          | ((']' :: _), _) -> false
+          | (('}' :: _), _) -> false
+          | ((_ :: st), stack) -> helper st stack
 
-      match ourString with
-      |_ when ourString = "" -> None
-      |_ when ourString.Length = helper -> if round = 0 && square = 0 && figured = 0 then Some true
-                                           else Some false
-      |_ when ourString.[helper] = '(' -> checkBraсkets ourString (helper + 1) (round + 1) square figured
-      |_ when ourString.[helper] = ')' -> if funHelper round then Some false
-                                          else checkBraсkets ourString (helper + 1) (round - 1) square figured   
-      |_ when ourString.[helper] = '{' -> checkBraсkets ourString (helper + 1) round (square + 1) figured
-      |_ when ourString.[helper] = '}' -> if funHelper square then Some false
-                                          else checkBraсkets ourString (helper + 1) round (square - 1) figured  
-      |_ when ourString.[helper] = '[' -> checkBraсkets ourString (helper + 1) round square (figured + 1)
-      |_ when ourString.[helper] = ']' -> if funHelper figured then Some false
-                                          else checkBraсkets ourString (helper + 1) round square (figured - 1)  
-      |_ -> checkBraсkets ourString (helper + 1) round  square figured   
+      helper [for x in myString -> x] []  
+
+  
