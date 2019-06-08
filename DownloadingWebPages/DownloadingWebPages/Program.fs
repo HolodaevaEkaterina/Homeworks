149dfb3043dcfb4689f1fe882e567345f553b62a
Ekaterina <kata_1998@mail.ru>
2019-06-09 00:12:16 +0300

    added notes

--- DownloadingWebPages/DownloadingWebPages/Program.fs
+++ DownloadingWebPages/DownloadingWebPages/Program.fs
@@ -5,30 +5,36 @@ open System.Net
 open Microsoft.FSharp.Control.WebExtensions
 open System.IO
 
+/// Downloads the page and all pages to which there are links
 let webPageLoading address =
-    
+
+    /// Asynchronous download
     let asyncComp(address : string) = 
          async {
+            try
                 let request = WebRequest.Create(address)
                 use! response = request.AsyncGetResponse()
                 use stream = response.GetResponseStream()
                 use reader = new StreamReader(stream)
                 let str = reader.ReadToEnd()
                 printfn "%A" (address + "—" +  str.Length.ToString())
-                return str
+                return Some str
+            with 
+            | _ -> return None     
             }
 
-    let readPage(url:string) =
-        async {
-            let! html = asyncComp url
-            return html
-          }
-    
-    let regex = new Regex("<a\shref=\x22https?://\w[\w-]*\.[a-zA-Z]+[^\x22]*\x22[^>]*>")
-    let html = Async.RunSynchronously(readPage address)
-    let webPages = regex.Matches(html)
-    let pageList = [for url in webPages -> 
-                       let value = url.Value
-                       asyncComp(value.Substring(value.IndexOf("f=") + 3 , value.IndexOf("\">") - value.IndexOf("=\"") - 2))]
-    Async.Parallel pageList |> Async.RunSynchronously |> ignore
-webPageLoading "https://vk.com/id137171441"
+    /// Search for regular expressions on each of the pages
+    let download content = 
+        let regex = new Regex("<a\shref=\x22https?://\w[\w-]*\.[a-zA-Z]+[^\x22]*\x22[^>]*>")
+        let webPages = regex.Matches(content)
+        let pageList = [for url in webPages -> 
+                           let value = url.Value
+                           asyncComp(value.Substring(value.IndexOf("f=") + 3 , value.IndexOf("\">") - value.IndexOf("=\"") - 2))]
+        Async.Parallel pageList |> Async.RunSynchronously |> Array.toList
+
+    let listPage = address |> asyncComp |> Async.RunSynchronously
+    match listPage with
+    | Some site -> listPage :: download site
+    | None -> [None]
+
+        
